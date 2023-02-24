c3e47a4c7426096c718edc5fc63f80be2e95cec5
Gabriel Teodoro <teodorogabriel367@gmail.com>
2023-02-24 19:26:19 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,68 @@
-var btn = document.getElementById('envio')
-btn.addEventListener('click', inserir)
+var btn = document.getElementById("run")
+btn.addEventListener("click", inserir)
 
-function inserir(){
+function inserir () {
 
-  let p1 =document.getElementById('page1')
-  let p2 = document.getElementById('page2')
+    //Muda  estilo da página
 
-  p1.style.display = "none"
-  p2.style.display = "flex"
+    let estilo = document.getElementById("estilo")
+    estilo.setAttribute("href", "css/main.css")
 
-  let codeHtml = document.getElementById('codeHtml').value
-  p2.innerHTML = codeHtml
+    // Troca o botão para reset
 
-  let codeJs = document.getElementById('codeJs').value
-  document.getElementById('js/script.js').innerHTML = codeJs
+    let header = document.getElementById("header")
+    header.innerHTML = `<input id="reset" type="button" value="Reset">`
+
+    // ocultar página 1 e exibir página 2
+
+    let pag1 = document.getElementById("page1")
+    let pag2 = document.getElementById("page2")    
+
+    pag1.style.display = "none"
+    pag2.style.display = "flex"
+
+    //inserir código em HTML
+
+    let codeHtml = document.getElementById("codeHtml").value
+    pag2.innerHTML = codeHtml
+
+    //inserir código em JavaScript
+
+    let codeJs = document.getElementById("codeJs").value
+    document.getElementById("jsUser").innerHTML = codeJs
+
+
+    //Botão de reset
+
+    var btn2 = document.getElementById("reset")
+    btn2.addEventListener("click", reset)
+
+    function reset () {
+
+        // ocultar página 1 e exibir página 2
+
+        let estilo = document.getElementById("estilo")
+        estilo.setAttribute("href", "css/style.css")
+
+        let pag1 = document.getElementById("page1")
+        let pag2 = document.getElementById("page2")    
     
+        pag1.style.display = "flex"
+        pag2.style.display = "none"
+
+        //Retorna o botão run
+
+        let header = document.getElementById("header")
+        header.innerHTML = `<input id="run" type="button" value="Run">`
 
 }
+}
+
+/*
+    let codeJs =document.getElementById("codeJs").value
+
+    var tagScript = document.createTextNode(codeJs)
+
+    tagScript.appendChild(textScript)
+    document.body.appendChild(tagScript)
+*/
